56d028fa335f32649739398c2da1e8408db419e4
gua <>
2018-01-26 23:51:20 +0800

    * 只有websocket 客户端并连接成功才push数据

--- src/Locker.php
+++ src/Locker.php
@@ -8,23 +8,30 @@ use Swoole\Lock;
 
 class Locker
 {
-    private static $_lockers = [];
+    private $_lockers = [];
+
+    public function __construct($names)
+    {
+        foreach ($names as $name) {
+            $this->_lockers[$name] = new Lock(SWOOLE_MUTEX);
+        }
+    }
 
     public function lock($name = 'default')
     {
-        if (!self::$_lockers[$name]) {
-            self::$_lockers[$name] = new Lock(SWOOLE_MUTEX);
+        if (!$this->_lockers[$name]) {
+            $this->_lockers[$name] = new Lock(SWOOLE_MUTEX);
         }
-        self::$_lockers[$name]->lock();
+        $this->_lockers[$name]->lock();
     }
 
     public function unlock($name = 'default')
     {
-        self::$_lockers[$name]->unlock();
+        $this->_lockers[$name]->unlock();
     }
 
     public function clear($name = 'default')
     {
-        unset(self::$_lockers[$name]);
+        unset($this->_lockers[$name]);
     }
 }
